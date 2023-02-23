44197bade0cee326adc12a98d0ff649627e45b8a
landruu <andres030685@gmail.com>
2023-02-23 10:25:40 -0300

    DecimalFormat.format(decimalNumber)

--- src/ConversorPackage/Main.java
+++ src/ConversorPackage/Main.java
@@ -1,23 +1,26 @@
 package ConversorPackage;
 
+import java.text.DecimalFormat;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		// aqui el código del conversor
 		
-		double currentMoney = 10;
+		double currentMoney = 23;
 		Currency dolar = Currency.USD;
 		Currency euro = Currency.EUR;
 		Currency libras = Currency.GBP;
 		Currency yanes = Currency.JPY;
 		Currency yen = Currency.KRW;
 		
-		
+		// DecimalFormat disminuye los decimales usando su funcion format();
+		DecimalFormat df = new DecimalFormat("#.00");
 		Money monedaActual = new Money(currentMoney, dolar);
 		Money monedaCambiada = monedaActual.convertTo(euro);
 		
 		System.out.println("Cambio: " + monedaActual.getAmount() + " " + monedaActual.getCurrency()
-							+ " = " + monedaCambiada.getAmount() + " " + monedaCambiada.getCurrency());
+							+ " = " + df.format(monedaCambiada.getAmount()) + " " + monedaCambiada.getCurrency());
 		
 	}
 
